37d775bad4f2235f63cdb187cd4ed78a0832691e
chubedan <chubedan@gmail.com>
2023-05-11 22:35:22 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,75 +5,19 @@ gulp.task("licenses", async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-
-      =========================================================
-      * Purity UI Dashboard - v1.0.1
-      =========================================================
-      
-      * Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
-      * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-      * Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
-      
-      * Design by Creative Tim & Coded by Simmmple
-      
-      =========================================================
-      
-      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-      
-      */`)
-    )
+    .pipe(gap.prependText(`hello`))
     .pipe(gulp.dest("./", { overwrite: true }));
 
   // this is to add Creative Tim licenses in the production mode for the minified html
   gulp
     .src("build/index.html", { base: "./" })
-    .pipe(
-      gap.prependText(`<!--
-      /*!
-      
-      =========================================================
-      * Purity UI Dashboard - v1.0.1
-      =========================================================
-      
-      * Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
-      * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-      * Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
-      
-      * Design by Creative Tim & Coded by Simmmple
-      
-      =========================================================
-      
-      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-      
-      */
-      -->`)
-    )
+    .pipe(gap.prependText(`hi`))
     .pipe(gulp.dest("./", { overwrite: true }));
 
   // this is to add Creative Tim licenses in the production mode for the minified css
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-
-      =========================================================
-      * Purity UI Dashboard - v1.0.1
-      =========================================================
-      
-      * Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
-      * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-      * Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
-      
-      * Design by Creative Tim & Coded by Simmmple
-      
-      =========================================================
-      
-      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-      
-      */`)
-    )
+    .pipe(gap.prependText(`thank you`))
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
 });
